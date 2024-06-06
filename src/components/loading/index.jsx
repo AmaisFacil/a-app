41f339678e5d75f624f42ab4847f6f8452b9f5ba
dutra-pc <joao.vitornl@gmail.com>
2024-06-05 23:08:49 -0300

    :lipstick: feat: Add color theme in loading component

--- src/components/loading/index.jsx
+++ src/components/loading/index.jsx
@@ -1,13 +1,16 @@
 import { ActivityIndicator } from 'react-native';
+import { useTheme } from 'styled-components';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
 import { Container, Text} from './styles';
 
 const Loading = ({text}) => {
+  const theme = useTheme();
+
   return (
     <Container>
-          <StatusBar backgroundColor={"#F6D03C"} barStyle="light-content"></StatusBar>
+          <StatusBar backgroundColor={theme.colors.primary} barStyle="light-content"></StatusBar>
           <ActivityIndicator size={30} color={"#363636"} />
           {text && <Text>{text}</Text>}
     </Container>
