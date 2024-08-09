215dc9e7607bc42eabcd403f3f65b93ab0cba2e7
dutra-pc <joao.vitornl@gmail.com>
2024-08-09 16:58:44 -0300

    :boom: fix: rollback savePreview page

--- src/screens/savePreview/index.jsx
+++ src/screens/savePreview/index.jsx
@@ -1,130 +1,84 @@
-import React, { useState, useRef, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useDispatch, useSelector } from 'react-redux';
 import { ScrollView } from 'react-native';
-//import { Video } from 'expo-av';
+import React, { useState } from 'react';
+import { Video } from 'expo-av';
 
+import { Container, Content, VideoContainer } from './styles';
 import { removeLocalRecord } from '../../actions/localRecord';
 import { createWebprove } from '../../actions/webprove';
 import generateHash from '../../utils/generateHash';
 import Backnav from '../../components/backnav';
-import { Container, Content } from './styles';
 import Button from '../../components/button';
 import Input from '../../components/input';
 
 const SavePreview = ({ route }) => {
-  const [videoInfo, setVideoInfo] = useState({ duration: null, size: null });
-  const [form, setForm] = useState({ details: "", name: "" });
+  const [form, setForm] = useState({details: "", name:""});
   const [status, setStatus] = useState("");
   const { save } = route.params;
-
+  
   const user = useSelector((state) => state.user);
   const { navigate } = useNavigation();
   const dispatch = useDispatch();
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    const getVideoInfo = async () => {
-      /*
-      if (videoRef.current) {
-        const status = await videoRef.current.getStatusAsync();
-        setVideoInfo({
-          duration: status.durationMillis,
-          size: null 
-        });
-      }
-        */
-    };
-    getVideoInfo();
-  }, [save.uri]);
 
   const handleCreateWebprove = async () => {
-    /*
     setStatus('loading');
     const hash = await generateHash(save.uri);
     const data = {
-      transactions: [{ transaction: false }, { transaction: false }],
-      timestamp: new Date().toUTCString(), 
+      transactions: [{transaction: false},{transaction: false}],
+      timestamp: new Date().toUTCString(Date.now()), 
       pinataTimestamp: Date.now(), 
       hash,
       app: null,
       email: user.email, 
       name: user.name, 
-      size: videoInfo.size || 0,
+      size: null,
       cid: hash, 
       form: {
         ...form,
         cpf: user.cpf,
         urls: "",
-        start: save.date,
-        stop: save.date + videoInfo.duration, 
-        duration: videoInfo.duration / 1000
+        start: null,
+        stop: null, 
+        duraction:  null
       }, 
-      pinata: {}, 
-      link: "", 
-    };
-
+      pinata:{}, 
+      link:"", 
+    }
     await createWebprove(data);
-    await removeLocalRecord(dispatch, save.uri);
+    await removeLocalRecord(dispatch, save.uri)
     setStatus('');
-    navigate('Webproves');
-    */
+    navigate('Webproves')
   };
-
   const handleDeleteWebprove = async () => {
     setStatus('loading');
-    await removeLocalRecord(dispatch, save.uri);
+    await removeLocalRecord(dispatch, save.uri)
     setStatus('');
-    navigate('Saves');
+    navigate('Saves')
   };
-  /*
-            <Video
-            ref={videoRef}
-            source={{ uri: save.uri }}
-            onLoad={(status) => {
-              setVideoInfo({
-                duration: status.durationMillis, 
-                size: null 
-              });
-            }}
-            style={{ width: '100%', height: 200 }} 
-            useNativeControls
-          />
+
+  return (
     <Container>
       <Backnav text='Visualizar Video Salvo'/>
       <Content>
         <ScrollView showsVerticalScrollIndicator={false}>
-
-          <Input 
-            placeholder='Nome do certificado' 
-            value={form.name} 
-            onChangeText={(x) => setForm({ ...form, name: x })} 
-            margin="10px 0" 
-            width={90} 
-          />
-          <Input 
-            placeholder='Descrição do certificado' 
-            value={form.details} 
-            onChangeText={(x) => setForm({ ...form, details: x })}  
-            margin="1px 0" 
-            width={90} 
-          />
-          <Button 
-            width={90} 
-            text='Gerar certificado' 
-            icon='file-plus' 
-            margin='10px 0' 
-            loading={status === 'loading'} 
-            onPress={handleCreateWebprove} 
-          />
+          <VideoContainer>
+            <Video
+              source={{ uri: save.uri }}
+              useNativeControls
+              resizeMode="contain"
+              style={{ width: '100%', height: 200 }} 
+            />
+          </VideoContainer>
+          <Input placeholder='nome do certificado' value={form.name} onChangeText={(x) => setForm({...form, name: x})} margin="10px 0" width={90}/>
+          <Input placeholder='descrição do certificado' value={form.details} onChangeText={(x) => setForm({...form, details: x})}  margin="1px 0" width={90}/>
+          <Button width={90} text='gerar certificado' icon='file-plus' margin='10px 0' loading={status == 'loading'} onPress={handleCreateWebprove}/>
         </ScrollView>
       </Content>
     </Container>
-  */
-
-  return (
-    <></>
   );
 };
 
 export default SavePreview;
+
+//          <Button width={90} text='apagar' icon='trash' margin='10px 0' loading={status == 'loading'} variant='error' onPress={handleDeleteWebprove}/>
